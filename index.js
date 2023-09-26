06f78fd2ecf9be7e31f31a43966b300a4d9a43cc
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-09-26 12:53:19 +0300

    Completed with assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(result => result.result === "W");//&& game.team === "Denver Broncos");
+   return win ? win.year : undefined;
+  }
+  
+
